0de1dfde26c734d9eb4dd61cad6603159b3ec5b7
TheNootLinja <thenootlinja@gmail.com>
2021-05-22 20:34:45 -0400

    Group pieces of post information into groups to be able to have them on the same lines where appropriate

--- frontend/src/components/Card/Card.js
+++ frontend/src/components/Card/Card.js
@@ -2,18 +2,26 @@ import './Card.styles.css';
 
 function Card({ post }) {
   return (
-    <div className="card">
-      <p className="sub">{post.postSub}</p>
-      <p className="author">u/{post.postAuthor}</p>
-      <p className="date">{post.postDate}</p>
-      <h3 className="title">{post.postTitle}</h3>
-      <div className="flair-container">
-        <span className="flair">{post.postFlair}</span>
+      <div className='card-container'>
+        <div className='card-left'>
+            <div className='card'>
+                <div className='info-container'>
+                    <p className="sub info">{post.postSub}</p>
+                    <p className="author info">Posted by u/{post.postAuthor}</p>
+                    <p className="date info">{post.postDate}</p>
+                </div>
+                <div className="title-container">
+                    <h3 className="title">{post.postTitle}</h3>
+                    <div className="flair-container">
+                        <span className="flair">{post.postFlair}</span>
+                    </div>
+                </div>
+                <div className="content-container">
+                    <p>{post.postContent}</p>
+                </div>
+            </div>
+        </div>
       </div>
-      <div className="content-container">
-        <p>{post.postContent}</p>
-      </div>
-    </div>
   );
 }
 
